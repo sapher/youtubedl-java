290f37626977e4eb649ffe8e3c61dfeef8bd9e2b
Severin Julien <sapher974@gmail.com>
2016-05-26 21:04:11 +0400

    add travic ci conf file

---
+++ src/test/java/com/sapher/youtubedl/TestTest.java
@@ -0,0 +1,12 @@
+package com.sapher.youtubedl;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class TestTest {
+
+    @Test
+    public void testTest() {
+        Assert.assertTrue(1 == 1);
+    }
+}
