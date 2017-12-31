e7d5fa8cfe61aabbff97ae3fe57e5fbf57d1b36a
Maximilian Hippler <info@hiiw.de>
2017-12-31 02:50:23 +0100

    Added ability to progress playlists

--- src/main/java/com/sapher/youtubedl/DownloadProgressCallback.java
+++ src/main/java/com/sapher/youtubedl/DownloadProgressCallback.java
@@ -1,5 +1,6 @@
 package com.sapher.youtubedl;
 
 public interface DownloadProgressCallback {
+    void onVideoUpdate(int videoNumber, int videoCount);
     void onProgressUpdate(float progress, long etaInSeconds);
 }
